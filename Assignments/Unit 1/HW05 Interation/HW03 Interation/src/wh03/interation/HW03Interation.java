d1994b4f629c9d24b83a5197c66ffc2efcca036e
Aaron Eduardo Cambi Chafla <75390416+Aaron-Cambi@users.noreply.github.com>
2021-01-14 19:41:31 -0500

    WH03  Interation

--- Assignments/Unit 1/HW05 Interation/HW03 Interation/src/wh03/interation/HW03Interation.java
+++ Assignments/Unit 1/HW05 Interation/HW03 Interation/src/wh03/interation/HW03Interation.java
@@ -16,19 +16,24 @@ public class HW03Interation {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n = 0;
+            int n = 0;
         System.out.println("First 11 Numbers");
-        while(n < 11){
-            System.out.println(" n " + "= " + (n+1));
+            while(n < 11){
+        System.out.println(" n " + "= " + (n+1));
             n++;
         }
+        System.out.println("Even Numbers");
+            int a = 0;
+            while(a < 11){
+        System.out.println("Even --> " + (a+=2));
+        }
         System.out.println("Multiplication table 11");
-        int stop=12;
-        int product = 0;
-        int table = 11; 
-        for(int m = 1; m <= stop; m++){
-            product = table * m;
-            System.out.println(" 11 " + "*" + m + " = " + product);
+            int stop=12;
+            int product = 0;
+            int table = 11; 
+            for(int m = 1; m <= stop; m++){
+                product = table * m;
+        System.out.println(" 11 " + "*" + m + " = " + product);
         }
         }
     }
